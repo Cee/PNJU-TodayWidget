1a2ce21198f65d969162bdb6dc84870240180fcc
Cee <cee@chu2byo.com>
2015-06-14 00:48:28 +0800

    Update Post Logic

--- PNJUWidget/NetworkManager.m
+++ PNJUWidget/NetworkManager.m
@@ -23,10 +23,10 @@
 
 - (NSString *)loginWithUsername:(NSString *)username password:(NSString *)password
 {
-    NSURL *url = [[NSURL alloc] initWithString:@"http://p.nju.edu.cn/portal/portal_io.do"];
+    NSURL *url = [[NSURL alloc] initWithString:@"http://p.nju.edu.cn/portal_io/login"];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
     [request setHTTPMethod:@"POST"];
-    NSString *actionString = [NSString stringWithFormat:@"action=login&username=%@&password=%@", username, password];
+    NSString *actionString = [NSString stringWithFormat:@"username=%@&password=%@", username, password];
     NSData *data = [actionString dataUsingEncoding:NSUTF8StringEncoding];
     [request setHTTPBody:data];
     NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
@@ -37,12 +37,9 @@
 
 - (void)logout
 {
-    NSURL *url = [[NSURL alloc] initWithString:@"http://p.nju.edu.cn/portal/portal_io.do"];
+    NSURL *url = [[NSURL alloc] initWithString:@"http://p.nju.edu.cn/portal_io/logout"];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
     [request setHTTPMethod:@"POST"];
-    NSString *actionString = @"action=logout";
-    NSData *data = [actionString dataUsingEncoding:NSUTF8StringEncoding];
-    [request setHTTPBody:data];
     NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
     NSString *responseStr = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
     NSLog(@"%@", responseStr);
@@ -50,7 +47,7 @@
 
 - (ConnectedStatus)checkOnline
 {
-    NSURL *url = [[NSURL alloc] initWithString:@"http://p.nju.edu.cn/proxy/online.php"];
+    NSURL *url = [[NSURL alloc] initWithString:@"http://p.nju.edu.cn/portal_io/proxy/userinfo"];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
     [request setHTTPMethod:@"POST"];
     NSError *error;
@@ -62,7 +59,7 @@
     NSLog(@"%@", responseStr);
     id json = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:nil];
     NSUInteger replyCodeString = [[json objectForKey:@"reply_code"] integerValue];
-    if (replyCodeString == 301) {
+    if (replyCodeString == 3010101) {
         return ConnectedStatusSuccess;
     } else {
         return ConnectedStatusFail;
@@ -71,12 +68,12 @@
 
 - (id)userInfo
 {
-    NSURL *url = [[NSURL alloc] initWithString:@"http://p.nju.edu.cn/proxy/online.php"];
+    NSURL *url = [[NSURL alloc] initWithString:@"http://p.nju.edu.cn/portal_io/proxy/userinfo"];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
     [request setHTTPMethod:@"POST"];
     NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
     id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-    json = [json objectForKey:@"userinfo"];
+    json = [json objectForKey:@"results"];
     return json;
 }
 
